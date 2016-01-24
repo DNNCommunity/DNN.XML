35c5a27b9f8b888679cb75e0f0c63351dc1503e4
Ernst Peter Tamminga <ernst.peter.tamminga@xcess.nl>
2016-01-24 10:12:25 +0100

    Creating a working build and packaging

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -47,7 +47,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("6.0.4.*")]
+[assembly: AssemblyVersion("6.0.4.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
+[assembly: AssemblyFileVersion("6.0.4.0")]
+
